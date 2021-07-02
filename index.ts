f8e73d3bf632721616c5f293a4350a51f6f2e9bf
dogeduaa <quzabubaker@gmail.com>
2021-07-02 21:08:40 +0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+const calculateDogYears = (dogAge: number) => {
+    return dogAge * 7;
+}
+
+const convertToHumanFromDog = (humanAge: number) => {
+    return humanAge / 7;
+}
+
+app.post('/api/dog-to-human', (req, res) => {
+    if (typeof req.body.age === "number") {
+        res.json({
+            status: 'OK',
+            age: calculateDogYears(req.body.age)
+        })
+    } else {
+        res.status(400).json({
+            status: 'User error',
+            message: 'Input must be a number'
+        })
+    }
+});
+
+app.post('/api/human-to-dog', (req, res) => {
+    if (typeof req.body.age === "number") {
+        res.json({
+            status: 'OK',
+            age: convertToHumanFromDog(req.body.age)
+        })
+    } else {
+        res.status(400).json({
+            status: 'User error',
+            message: 'Input must be a number'
+        })
+    }
+});
+
+const port: number = Number.parseInt(process.env.PORT as string) || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+});
